a25d8f3ca1041f4df8ddaad03ab9583793f2d836
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-07-20 17:31:30 -0400

    import initial mods xmlmap objects & tests

---
+++ eulxml/xmlmap/mods.py
@@ -0,0 +1,234 @@
+# file eulxml/xmlmap/mods.py
+# 
+#   Copyright 2010,2011 Emory University Libraries
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+'''
+:mod:`eulxml.xmlmap` classes for dealing with the
+`MODS <http://www.loc.gov/standards/mods/>`_ metadata format
+(Metadata Object Description Schema).
+'''
+
+from eulxml import xmlmap
+
+MODS_NAMESPACE = 'http://www.loc.gov/mods/v3'
+MODS_SCHEMA = "http://www.loc.gov/standards/mods/mods.xsd"
+MODSv34_SCHEMA = "http://www.loc.gov/standards/mods/v3/mods-3-4.xsd"
+
+# MODS schema is loaded by several of the mods classes - load once and reuse
+_mods_xmlschema = xmlmap.loadSchema(MODS_SCHEMA)
+
+class Common(xmlmap.XmlObject):
+    "MODS class with namespace declaration common to all MODS XmlObjects."
+    ROOT_NS = MODS_NAMESPACE
+    ROOT_NAMESPACES = {'mods': MODS_NAMESPACE }
+
+class Date(Common):
+    ''':class:`~eulxml.xmlmap.XmlObject` for MODS date element (common fields
+    for the dates under mods:originInfo).'''
+    # this class not meant for direct use; should be extended for specific dates.
+    # FIXME: schema required here for schemafields; this should be refactored
+    XSD_SCHEMA = MODS_SCHEMA
+    xmlschema = _mods_xmlschema
+    date = xmlmap.StringField('text()')
+    key_date = xmlmap.SimpleBooleanField('@keyDate', 'yes', false=None)
+    encoding = xmlmap.SchemaField('@encoding', 'dateEncodingAttributeDefinition')
+    point = xmlmap.SchemaField('@point', 'datePointAttributeDefinition')
+    qualifier = xmlmap.SchemaField('@qualifier', 'dateQualifierAttributeDefinition')
+
+    def is_empty(self):
+        '''Returns False if no date value is set; returns True if any date value
+        is set.  Attributes are ignored for determining whether or not the
+        date should be considered empty, as they are only meaningful in
+        reference to a date value.'''
+        return not self.node.text
+
+class DateCreated(Date):
+    ROOT_NAME = 'dateCreated'
+
+class DateIssued(Date):
+    ROOT_NAME = 'dateIssued'
+
+class OriginInfo(Common):
+    ":class:`~eulxml.xmlmap.XmlObject` for MODS originInfo element (incomplete)"
+    ROOT_NAME = 'originInfo'
+    created = xmlmap.NodeListField('mods:dateCreated', DateCreated,
+        verbose_name='Date Created',
+        help_text='Date the resource was first created (e.g., date of recording,' +
+            ' photograph taken, or letter written)')
+    issued = xmlmap.NodeListField('mods:dateIssued', DateIssued,
+        verbose_name='Date Issued',
+        help_text='Date the resource was published, released, or issued')
+
+    def is_empty(self):
+        """Returns True if all child date elements present are empty.  Returns
+        False if any child date elements are not empty."""
+        return all(date.is_empty() for date in set.union(set(self.created), set(self.issued)))
+
+class RecordInfo(Common):
+    ROOT_NAME = 'recordInfo'
+    record_id = xmlmap.StringField('mods:recordIdentifier')
+    record_origin = xmlmap.StringField('mods:recordOrigin')
+    creation_date = xmlmap.StringField('mods:recordCreationDate[@encoding="w3cdtf"]')
+    change_date = xmlmap.StringField('mods:recordChangeDate[@encoding="w3cdtf"]')
+
+class Note(Common):
+    ":class:`~eulxml.xmlmap.XmlObject` for MODS note element"
+    ROOT_NAME = 'note'
+    label = xmlmap.StringField('@displayLabel')
+    type = xmlmap.StringField('@type')
+#                choices=['general', 'inscription', 'source of information',
+#                        'reference', 'hidden'])
+        # with capacity to add to the list ?
+        # FIXME: keep-specific note type choices should not be defined here
+    text = xmlmap.StringField('text()')      # actual text value of the note
+
+class TypedNote(Note):
+    '''Extends :class:`Note` to modify :meth:`is_empty` behavior-- considered
+    empty when a type attribute is set without any text.'''
+    
+    def is_empty(self):
+        """Returns True if the root node contains no child elements, no text,
+        and no attributes other than **type**. Returns False if any are present."""
+        non_type_attributes = [attr for attr in self.node.attrib.keys() if attr != 'type']
+        return len(self.node) == 0 and len(non_type_attributes) == 0 \
+            and not self.node.text and not self.node.tail
+
+
+class Identifier(Common):
+    ':class:`~eulxml.xmlmap.XmlObject` for MODS identifier'
+    ROOT_NAME = 'identifier'
+    type = xmlmap.StringField('@type')
+    text = xmlmap.StringField('text()')
+
+class AccessCondition(Common):
+    ':class:`~eulxml.xmlmap.XmlObject` for MODS accessCondition'
+    ROOT_NAME = 'accessCondition'
+    type = xmlmap.StringField('@type',
+            choices=['restrictions on access', 'use and reproduction'])
+    text = xmlmap.StringField('text()')
+
+class NamePart(Common):
+    ':class:`~eulxml.xmlmap.XmlObject` for MODS namePart'
+    ROOT_NAME = 'namePart'
+    # FIXME: schema required here for schemafields; this should be refactored
+    XSD_SCHEMA = MODS_SCHEMA
+    xmlschema = _mods_xmlschema
+
+    type = xmlmap.SchemaField('@type', 'namePartTypeAttributeDefinition',
+                              required=False) # type is optional
+    text = xmlmap.StringField('text()')
+
+class Role(Common):
+    ':class:`~eulxml.xmlmap.XmlObject` for MODS role'
+    ROOT_NAME = 'role'
+    type = xmlmap.StringField('mods:roleTerm/@type')
+    authority = xmlmap.StringField('mods:roleTerm/@authority', choices=['', 'marcrelator', 'local'])
+    text = xmlmap.StringField('mods:roleTerm')
+
+class Name(Common):
+    ':class:`~eulxml.xmlmap.XmlObject` for MODS name'
+    ROOT_NAME = 'name'
+    # FIXME: schema required here for schemafields; this should be refactored
+    XSD_SCHEMA = MODS_SCHEMA
+    xmlschema = _mods_xmlschema
+
+    type = xmlmap.SchemaField('@type', 'nameTypeAttributeDefinition', required=False)
+    authority = xmlmap.StringField('@authority', choices=['', 'local', 'naf'], required=False) # naf = NACO authority file
+    id = xmlmap.StringField('@ID', required=False)  # optional
+    name_parts = xmlmap.NodeListField('mods:namePart', NamePart)
+    display_form = xmlmap.StringField('mods:displayForm')
+    affiliation = xmlmap.StringField('mods:affiliation')
+    roles = xmlmap.NodeListField('mods:role', Role)
+
+    def __unicode__(self):
+        # default text display of a name (excluding roles for now)
+        # TODO: improve logic for converting to plain-text name
+        # (e.g., for template display, setting as dc:creator, etc)
+        return ' '.join([unicode(part) for part in self.name_parts])
+
+class Genre(Common):
+    ROOT_NAME = 'genre'
+    authority = xmlmap.StringField('@authority')
+    text = xmlmap.StringField('text()')
+
+class LanguageTerm(Common):
+    ROOT_NAME = 'languageTerm'
+    type = xmlmap.StringField('@type')
+    authority = xmlmap.StringField('@authority')
+    text = xmlmap.StringField('text()')
+
+class Language(Common):
+    ROOT_NAME = 'language'
+    terms = xmlmap.NodeListField('mods:languageTerm', LanguageTerm)
+
+class Subject(Common):
+    ROOT_NAME = 'subject'
+    authority = xmlmap.StringField('@authority')
+
+    # and one of the following:
+    geographic = xmlmap.StringField('mods:geographic')
+    name = xmlmap.NodeField('mods:name', Name)
+    topic = xmlmap.StringField('mods:topic')
+    title = xmlmap.StringField('mods:titleInfo/mods:title')
+
+class BaseMods(Common):
+    ''':class:`~eulxml.xmlmap.XmlObject` with common field declarations for all
+    top-level MODS elements; base class for :class:`MODS` and :class:`RelatedItem`.'''
+    XSD_SCHEMA = MODS_SCHEMA
+    xmlschema = _mods_xmlschema
+
+    title = xmlmap.StringField("mods:titleInfo/mods:title")
+    resource_type  = xmlmap.SchemaField("mods:typeOfResource", "resourceTypeDefinition")
+    name = xmlmap.NodeField('mods:name', Name)  # DEPRECATED: use names instead
+    names = xmlmap.NodeListField('mods:name', Name)
+    note = xmlmap.NodeField('mods:note', Note)
+    origin_info = xmlmap.NodeField('mods:originInfo', OriginInfo)
+    record_info = xmlmap.NodeField('mods:recordInfo', RecordInfo)
+    identifiers = xmlmap.NodeListField('mods:identifier', Identifier)
+    access_conditions = xmlmap.NodeListField('mods:accessCondition', AccessCondition)
+    genres = xmlmap.NodeListField('mods:genre', Genre)
+    languages = xmlmap.NodeListField('mods:language', Language)
+    location = xmlmap.StringField('mods:location/mods:physicalLocation',
+                                  required=False)
+    subjects = xmlmap.NodeListField('mods:subject', Subject)
+
+class RelatedItem(BaseMods):
+    ''':class:`~eulxml.xmlmap.XmlObject` for MODS relatedItem: contains all the
+    top-level MODS fields defined by :class:`BaseMods`, plus a type attribute.'''
+    ROOT_NAME = 'relatedItem'
+    # FIXME: schema required here for schemafields; this should be refactored
+    XSD_SCHEMA = MODS_SCHEMA
+    xmlschema = _mods_xmlschema
+    type = xmlmap.SchemaField("@type", 'relatedItemTypeAttributeDefinition')
+
+class MODS(BaseMods):
+    '''Top-level :class:`~eulxml.xmlmap.XmlObject` for a MODS metadata record.
+    Inherits all standard top-level MODS fields from :class:`BaseMods` and adds
+    a mapping for :class:`RelatedItem`.
+    '''
+    ROOT_NAME = 'mods'
+    XSD_SCHEMA = MODS_SCHEMA
+    xmlschema = _mods_xmlschema
+    related_items = xmlmap.NodeListField('mods:relatedItem', RelatedItem)
+
+
+class MODSv34(MODS):
+    ''':class:`~eulxml.xmlmap.XmlObject` for MODS version 3.4.  Currently
+    consists of all the same fields as :class:`MODS`, but loads the MODS version
+    3.4 schema for validation.
+    '''
+    XSD_SCHEMA = MODSv34_SCHEMA
+    xmlschema = xmlmap.loadSchema(XSD_SCHEMA)
+    # FIXME: how to set version attribute when creating from scratch?
---
+++ test/test_xmlmap/test_mods.py
@@ -0,0 +1,219 @@
+# file test_xmlmap/test_mods.py
+# 
+#   Copyright 2011 Emory University Libraries
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#!/usr/bin/env python
+
+import unittest
+
+from eulxml.xmlmap import load_xmlobject_from_string, mods
+from testcore import main
+
+class TestMods(unittest.TestCase):
+    # tests for MODS XmlObject
+
+    FIXTURE = """<mods:mods xmlns:mods="http://www.loc.gov/mods/v3">
+  <mods:titleInfo>
+    <mods:title>A simple record</mods:title>
+  </mods:titleInfo>
+  <mods:typeOfResource>text</mods:typeOfResource>
+  <mods:note displayLabel="a general note" type="general">remember to...</mods:note>
+  <mods:originInfo>
+    <mods:dateCreated keyDate='yes'>2010-06-17</mods:dateCreated>
+  </mods:originInfo>
+  <mods:identifier type='uri'>http://so.me/uri</mods:identifier>
+  <mods:name type="personal" authority="naf" ID="n82032703">
+    <mods:namePart>Dawson, William Levi</mods:namePart>
+    <mods:namePart type="date">1899-1990</mods:namePart>
+    <mods:displayForm>William Levi Dawson (1899-1990)</mods:displayForm>
+    <mods:affiliation>Tuskegee</mods:affiliation>
+    <mods:role>
+      <mods:roleTerm type="text" authority="marcrelator">Composer</mods:roleTerm>
+    </mods:role>
+  </mods:name>
+  <mods:accessCondition type="restrictions on access">Restricted</mods:accessCondition>
+  <mods:relatedItem type="host">
+    <mods:titleInfo>
+      <mods:title>Emory University Archives</mods:title>
+    </mods:titleInfo>
+  <mods:identifier type="local_sourcecoll_id">eua</mods:identifier>
+  </mods:relatedItem>
+</mods:mods>
+"""
+    invalid_xml = """<mods:mods xmlns:mods="http://www.loc.gov/mods/v3">
+    <mods:titleInfo><mods:title invalid_attribute='oops'>An invalid record</mods:title></mods:titleInfo>
+</mods:mods>
+        """
+
+    def setUp(self):
+        super(TestMods, self).setUp()
+        self.mods = load_xmlobject_from_string(self.FIXTURE, mods.MODS)
+
+    def test_init_types(self):
+        self.assert_(isinstance(self.mods, mods.MODS))
+        self.assert_(isinstance(self.mods.note, mods.Note))
+        self.assert_(isinstance(self.mods.origin_info, mods.OriginInfo))
+        self.assert_(isinstance(self.mods.origin_info.created[0], mods.DateCreated))
+        self.assert_(isinstance(self.mods.identifiers[0], mods.Identifier))
+        self.assert_(isinstance(self.mods.name, mods.Name))
+        self.assert_(isinstance(self.mods.name.name_parts[0], mods.NamePart))
+        self.assert_(isinstance(self.mods.name.roles[0], mods.Role))
+        self.assert_(isinstance(self.mods.access_conditions[0], mods.AccessCondition))
+        self.assert_(isinstance(self.mods.related_items[0], mods.RelatedItem))
+
+    def test_fields(self):
+        self.assertEqual('A simple record', self.mods.title)
+        self.assertEqual('text', self.mods.resource_type)
+        self.assertEqual('a general note', self.mods.note.label)
+        self.assertEqual('general', self.mods.note.type)
+        self.assertEqual(u'remember to...', unicode(self.mods.note))
+        self.assertEqual('remember to...', self.mods.note.text)
+        self.assertEqual(u'2010-06-17', unicode(self.mods.origin_info.created[0]))
+        self.assertEqual('2010-06-17', self.mods.origin_info.created[0].date)
+        self.assertEqual(True, self.mods.origin_info.created[0].key_date)
+        self.assertEqual(u'http://so.me/uri', self.mods.identifiers[0].text)
+        self.assertEqual(u'uri', self.mods.identifiers[0].type)
+        # name fields
+        self.assertEqual(u'personal', self.mods.name.type)
+        self.assertEqual(u'naf', self.mods.name.authority)
+        self.assertEqual(u'n82032703', self.mods.name.id)
+        self.assertEqual(u'Dawson, William Levi', self.mods.name.name_parts[0].text)
+        self.assertEqual(u'1899-1990', self.mods.name.name_parts[1].text)
+        self.assertEqual(u'date', self.mods.name.name_parts[1].type)
+        self.assertEqual(u'William Levi Dawson (1899-1990)', self.mods.name.display_form)
+        self.assertEqual(u'Tuskegee', self.mods.name.affiliation)
+        self.assertEqual(u'text', self.mods.name.roles[0].type)
+        self.assertEqual(u'marcrelator', self.mods.name.roles[0].authority)
+        self.assertEqual(u'Composer', self.mods.name.roles[0].text)
+        # access condition
+        self.assertEqual(u'restrictions on access', self.mods.access_conditions[0].type)
+        self.assertEqual(u'Restricted', self.mods.access_conditions[0].text)
+        # related item
+        self.assertEqual(u'host', self.mods.related_items[0].type)
+        self.assertEqual(u'Emory University Archives', self.mods.related_items[0].title)
+        self.assertEqual(u'local_sourcecoll_id', self.mods.related_items[0].identifiers[0].type)
+        self.assertEqual(u'eua', self.mods.related_items[0].identifiers[0].text)
+
+    def test_create_mods(self):
+        # test creating MODS from scratch - ensure sub-xmlobject definitions are correct
+        # and produce schema-valid MODS
+        mymods = mods.MODS()
+        mymods.title = 'A Record'
+        mymods.resource_type = 'text'
+        mymods.create_name()
+        mymods.name.type = 'personal'
+        mymods.name.authority = 'local'
+        mymods.name.name_parts.extend([mods.NamePart(type='family', text='Schmoe'),
+                                    mods.NamePart(type='given', text='Joe')])
+        mymods.name.roles.append(mods.Role(type='text', authority='local',
+                                        text='Test Subject'))
+        mymods.create_note()
+        mymods.note.type = 'general'
+        mymods.note.text = 'general note'
+        mymods.create_origin_info()
+        mymods.origin_info.created.append(mods.DateCreated(date='2001-10-02'))
+        mymods.origin_info.issued.append(mods.DateIssued(date='2001-12-01'))
+        mymods.create_record_info()
+        mymods.record_info.record_id = 'id:1'
+        mymods.identifiers.extend([mods.Identifier(type='uri', text='http://ur.l'),
+                                 mods.Identifier(type='local', text='332')])
+        mymods.access_conditions.extend([mods.AccessCondition(type='restriction', text='unavailable'),
+                                       mods.AccessCondition(type='use', text='Tuesdays only')])
+        mymods.related_items.extend([mods.RelatedItem(type='host', title='EU Archives'),
+                                   mods.RelatedItem(type='isReferencedBy', title='Finding Aid'),])
+        xml = mymods.serialize(pretty=True)
+        self.assert_('<mods:mods ' in xml)
+        self.assert_('xmlns:mods="http://www.loc.gov/mods/v3"' in xml)
+
+        self.assertTrue(mymods.is_valid(), "MODS created from scratch should be schema-valid")
+
+    def test_isvalid(self):
+        # if additions to MODS test fixture cause validation errors, uncomment the next 2 lines to debug
+        #self.mods.is_valid()
+        #print self.mods.validation_errors()
+        self.assertTrue(self.mods.is_valid())        
+        invalid_mods = load_xmlobject_from_string(self.invalid_xml, mods.MODS)
+        self.assertFalse(invalid_mods.is_valid())
+
+class TestModsTypedNote(unittest.TestCase):
+    # node fields tested in main mods test case; testing custom is_empty logic here
+
+    def setUp(self):
+        super(TestModsTypedNote, self).setUp()
+        self.note = mods.TypedNote()
+        self.note.type = 'general'
+
+    def test_is_empty(self):
+        # initial note object should be considered empty (type only)
+        self.assertTrue(self.note.is_empty())
+
+    def test_is_empty__extra_attribute(self):
+        # set an attribute besides type
+        self.note.label = "Note"        
+        self.assertFalse(self.note.is_empty())
+
+    def test_is_empty_text(self):
+        # set text value
+        self.note.text = 'here is some general info'
+        self.assertFalse(self.note.is_empty())
+
+class TestModsDate(unittest.TestCase):
+    # node fields tested in main mods test case; testing custom is_empty logic here
+
+    def setUp(self):
+        super(TestModsDate, self).setUp()
+        self.date = mods.DateCreated() 
+
+    def test_is_empty(self):
+        # starting fixture should be considered empty (no date)
+        self.assertTrue(self.date.is_empty())
+
+    def test_is_empty_with_attributes(self):
+        # should be empty with attributes but no date value
+        self.date.keydate = True
+        self.assertTrue(self.date.is_empty())
+
+    def test_is_empty_date_value(self):
+        # set date value
+        self.date.date = '1066'
+        self.assertFalse(self.date.is_empty())
+
+class TestModsOriginInfo(unittest.TestCase):
+    # node fields tested in main mods test case; testing custom is_empty logic here
+
+    def setUp(self):
+        super(TestModsOriginInfo, self).setUp()
+        self.origin_info = mods.OriginInfo()
+
+    def test_is_empty(self):
+        # starting object should be considered empty (no date elements at all)
+        self.assertTrue(self.origin_info.is_empty())
+
+    def test_is_empty_with_empty_dates(self):
+        self.origin_info.created.append(mods.DateCreated())
+        self.assertTrue(self.origin_info.is_empty())
+        self.origin_info.issued.append(mods.DateIssued())
+        self.assertTrue(self.origin_info.is_empty())
+
+    def test_is_empty_date_values(self):
+        self.origin_info.created.append(mods.DateCreated(date='300'))
+        self.assertFalse(self.origin_info.is_empty())
+        self.origin_info.issued.append(mods.DateIssued(date='450'))
+        self.assertFalse(self.origin_info.is_empty())
+
+
+
+if __name__ == '__main__':
+    main()
