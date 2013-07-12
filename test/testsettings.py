81c763cd8e0973456ed093906a3813fe8ed1c78d
Alex Thomas <athom09@emory.edu>
2013-07-12 14:47:56 -0400

    added non-secret SECRET_KEY

--- test/testsettings.py
+++ test/testsettings.py
@@ -19,3 +19,4 @@
 # eulxml.forms code relies on django code, which can't be imported
 # without a settings file configured, even though we don't actually
 # rely on any of those settings.
+SECRET_KEY = 'notreallythatbigofasecret'
