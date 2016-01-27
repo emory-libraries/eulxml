dec8d18cee12309154fece78e5a5e95946eebfa6
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2016-01-27 10:14:39 -0500

    Update setup module, tox and Travis files

--- setup.py
+++ setup.py
@@ -22,6 +22,13 @@ CLASSIFIERS = [
     'Natural Language :: English',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Text Processing :: Markup :: XML',
 ]
@@ -59,7 +66,7 @@ setup(
         'dev': [
             'sphinx',
             'coverage',
-            'Django<1.7',
+            'Django',
             'rdflib>=3.0',
             'mock',
             'nose',
